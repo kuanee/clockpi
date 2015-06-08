ad1a0056e37e0a3513852a3d99f71dd0b46cfe21
kuanee <kuanee@users.noreply.github.com>
2015-06-08 22:04:34 +0800

    initial project version

---
+++ gpiotest.py
@@ -0,0 +1,26 @@
+﻿import RPi.GPIO as GPIO
+import time
+
+output = 14
+
+# Set GPIO Mode to use BCM numbering
+GPIO.setmode(GPIO.BCM)
+
+# Set GPIO to input mode
+GPIO.setup(output, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+previous_state = False
+current_state = False
+
+while True:
+	time.sleep(0.1)
+	previous_state = current_state
+	current_state =GPIO.input(output)
+	if current_state != previous_state:
+		if not(current_state):
+			print("Motion Detected")
+		else:
+			print("No Movement")
+
+GPIO.cleanup()
+
